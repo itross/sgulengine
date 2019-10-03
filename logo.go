40be4d38f90d835789eaaef0a6aa2c7b32f2a0ce
Luca Stasio <joshuagame@gmail.com>
2019-10-03 16:07:34 +0200

    print logo

--- logo.go
+++ logo.go
@@ -4,6 +4,6 @@ import "github.com/common-nighthawk/go-figure"
 
 // PrintLogo prints out the SgulENGINE logo.
 func PrintLogo() {
-	myFigure := figure.NewFigure("SgulENGINE", "doom", true).Scroll(10000, 200, "left")
+	myFigure := figure.NewFigure("SgulENGINE", "doom", true)
 	myFigure.Print()
 }
